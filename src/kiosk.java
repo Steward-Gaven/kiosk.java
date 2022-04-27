313b45bd66f810d7f7450247cf2705db0b2f6b00
Steward-Gaven <Stewarwn@mail.uc.edu>
2022-04-27 11:04:55 -0400

    Initial commit

---
+++ src/kiosk.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        int age = 22;
+
+        if (age >= 21) {
+            System.out.println("You are 21 or over, enjoy a wristband");
+            ;
+        }
+    }
+}
